0d6f64c93661707a34172a87da4121ba0ec47c90
Dave Reed <dave256@mac.com>
2023-01-06 11:41:33 -0500

    Initial Commit

---
+++ MyContacts/MyContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyContactsApp.swift
+//  MyContacts
+//
+//  Created by David Reed on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MyContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
