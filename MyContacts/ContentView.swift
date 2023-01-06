0b7da633ed6d01765552e61598fa7a31e882a293
Dave Reed <dave256@mac.com>
2023-01-06 13:15:58 -0500

    use PreferenceKey to force cells to be same height

--- MyContacts/ContentView.swift
+++ MyContacts/ContentView.swift
@@ -9,12 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var app: AppModel
+    /// for tracking size of cells via preference key
+    @State private var cellHeight: CGFloat?
 
     var body: some View {
         NavigationStack {
             List {
                 ForEach(app.contacts) { c in
+                    // make cells all the same height even if some Text's are blank
                     ContactCell(contact: c)
+                        .foregroundColor(.primary)
+                        .frame(height: cellHeight)
+                        .sharedHeightUsingMax {
+                            cellHeight = $0
+                        }
+                        .padding([.top, .bottom], 2)
                 }
             }
             .navigationTitle("Contacts")
