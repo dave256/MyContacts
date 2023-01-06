d58707666a8c72fd330c261abe3cc499b5ed07d4
Dave Reed <dave256@mac.com>
2023-01-06 12:09:57 -0500

    initial ContactCell view

---
+++ MyContacts/ContactCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContactCell.swift
+//  MyContacts
+//
+//  Created by David Reed on 1/6/23.
+//
+
+import SwiftUI
+
+struct ContactCell: View {
+
+    var contact: Contact
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(contact.fullName)
+            Text(contact.email)
+            Text(contact.phone)
+        }
+    }
+}
+
+struct ContactCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactCell(contact: .mock)
+    }
+}
