39b31db835dbffe96a61054443c1049b5adb5361
Dave Reed <dave256@mac.com>
2023-01-06 14:47:52 -0500

    add loading/storing of contacts from/to JSON

--- MyContacts/MyContactsApp.swift
+++ MyContacts/MyContactsApp.swift
@@ -9,14 +9,36 @@ import SwiftUI
 
 @main
 struct MyContactsApp: App {
+
     // initial model
-    #warning("loading with sample data during development")
-    @State private var app = AppModel(contacts: .mock)
+    @StateObject private var app = AppModel()
+
+    // can re-insert these anytime we want to start fresh with the mock data
+    // and then onece put app in background, they will save and could comment out again
+    //    #warning("loading with sample data during development")
+    //    @State private var app = AppModel(contacts: .mock)
+
+    
+    // for detecting when app goes to background (Home button or swipe up to dimiss app)
+    // note pressing the stop button in Xcode quits immediately and it wont' save
+    @Environment(\.scenePhase) var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(app)
+
+                .onChange(of: scenePhase) { phase in
+                    switch phase {
+
+                    case .background:
+                        // when app enters background, save the contacts to JSON file
+                        app.storeContacts()
+
+                    default:
+                        break
+                    }
+                }
         }
     }
 }
